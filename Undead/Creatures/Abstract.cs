df8cf1f1514427ecc7cebe5a30aa82d453df485f
Nikolay Kolpakov <nik.vik.kolpakov@gmail.com>
2018-06-16 14:48:36 +0500

    add abstract undead class

---
+++ Undead/Creatures/Abstract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undead.Creatures
+{
+    abstract class Abstract
+    {
+        public string Name { get; set; }
+        public string Rank { get; set; }
+        public int Power { get; set; }
+        public int Mana { get; set; }
+        public int Health { get; set; }
+        public virtual string Scream()
+        {
+            return Name;
+        }
+        public virtual string Ability()
+        {
+            return "BaseAbility";
+        }
+    }
+}
---
+++ Undead/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Undead.Creatures;
+
+namespace Undead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Abstract> Creatures = new List<Abstract>();
+            Console.ReadKey();
+        }
+    }
+}
